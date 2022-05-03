cb464efb54870781faef98c7608747b9deb214f5
samlarson94 <samlarson94@gmail.com>
2022-05-02 20:58:36 -0500

    Updating category api routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll( {
+      // include: [] --> Add Products
+    });
+    console.log("Get Route Retrieved Successfully");
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+    console.log("Something went wrong");
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+
+  }
+ 
 });
 
 router.post('/', (req, res) => {
